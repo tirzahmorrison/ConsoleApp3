9bd24251d2112d8c2a1fb5ed42e4403857db4f4e
Tirzah Morrison <tirzahmorrison@icloud.com>
2018-04-04 22:44:54 -0400

    mighty morphin power rangers

---
+++ ConsoleApp3/MorseMapper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO; 
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseCode
+{
+    class MorseMapper
+    {
+        private Dictionary<string, string> morseMap = new Dictionary<string, string>();
+        private Dictionary<string, string> reverseMorseMap = new Dictionary<string, string>();
+
+        public MorseMapper(string path)
+        {
+            var fullPath = Path.GetFullPath(path);
+            var lines = File.ReadAllLines(fullPath);
+            foreach(var line in lines)
+            {
+                var parts = line.Split(',');
+                morseMap.Add(parts[0].ToLower(), parts[1]);
+                reverseMorseMap.Add(parts[1], parts[0].ToLower());
+            }
+        }
+
+        public String AlphaMorph(string mightyMorpher)
+        {
+            return morph(mightyMorpher, morseMap);
+        }
+
+        public String MorseMorph(string mightyMorpher)
+        {
+            return morph(mightyMorpher, reverseMorseMap);
+
+        }
+        private String morph(String mightyMorpher, Dictionary<string, string> ranger)
+        {
+            var result = new List<string>();
+            foreach(var c in mightyMorpher)
+            {
+                if (ranger.ContainsKey(c.ToString().ToLower())) {
+                    result.Add(ranger[c.ToString().ToLower()]);
+                    
+                }
+                else
+                {
+                    result.Add(c.ToString());
+                }
+            }
+            return String.Join("", result); 
+
+
+
+        }
+    }
+}
--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO; 
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp3
+namespace MorseCode
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var Mapper = new MorseMapper(args[0]);
+            var input = "";
+
+            while (true)
+            {
+                Console.WriteLine("Type the sentence you would like to convert to morse. Press Enter to quit.");
+                input = Console.ReadLine();
+                if (input == "")
+                {
+                    break;
+                }
+                var converted = Mapper.AlphaMorph(input);
+                Console.WriteLine("Your sentence has been encrypted to {0}", converted);
+                Console.WriteLine("If you want to continue type y if you want to quit type n");
+                input = Console.ReadLine();
+                if(input.ToLower() == "n")
+                {
+                    break;
+                }
+            } 
+
         }
     }
 }
